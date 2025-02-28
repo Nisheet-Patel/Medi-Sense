006d850e7bb7042a06b761cc8d624a7914145a55
harsh <harshsonegrahere@gmail.com>
2025-03-01 02:17:35 +0530

    sepsis model added

--- server_python/model.py
+++ server_python/model.py
@@ -7,3 +7,7 @@ diabetesModel = joblib.load('./PKL/diabetes_model.pkl')
 stressModel = joblib.load('./PKL/stress_model.pkl')
 
 model = tf.keras.models.load_model('./PKL/model.h5')
+
+sepsiModel = joblib.load('./PKL/sepsis.pkl')
+
+sepsisScaler = joblib.load('./PKL/scaler.pkl')
